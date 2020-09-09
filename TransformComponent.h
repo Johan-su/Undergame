71b0b63d7aea7ee82460ffcf8a9b9ad4568ed70b
Johan Su <jsbollll@gmail.com>
2020-09-09 11:44:51 +0200

    added vecf, transformComponent

---
+++ TransformComponent.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "vecf.h"
+
+
+struct TransformComponent
+{
+	Vec2f position;
+	Vec2f size;
+	Vec2f velocity;
+	float speed;
+	float rotation;
+};
---
+++ vecf.h
@@ -0,0 +1,13 @@
+#pragma once
+struct Vec2f
+{
+	float x, y;
+};
+struct Vec3f
+{
+	float x, y, z;
+};
+struct Vec4f
+{
+	float x, y, z, w;
+};
