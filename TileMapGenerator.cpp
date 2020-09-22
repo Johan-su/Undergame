16292a7b4f73f2af46dcfb1cf840278479afa2db
Johan Su <jsbollll@gmail.com>
2020-09-22 13:31:05 +0200

    tilemapgenerator

--- TileMapGenerator.cpp
+++ TileMapGenerator.cpp
@@ -2,5 +2,14 @@
 
 TileMap* TileMapGenerator::create_map_random(const unsigned long long& seed )
 {
+	auto tilemap = new TileMap();
+
+	srand(seed);
+	for (int i = 0; i < tilemap->grid.size(); ++i)
+	{
+
+		tilemap->grid[i];
+
+	}
 
 }
